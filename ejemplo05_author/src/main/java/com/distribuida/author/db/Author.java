e1d72c3e3b95b84f1a1b0c63ae6ee336ca32c780
easolano98 <solano_erick@hotmail.com>
2024-06-19 17:51:23 -0500

    Modulo Autor

---
+++ ejemplo05_author/src/main/java/com/distribuida/author/db/Author.java
@@ -0,0 +1,57 @@
+package com.distribuida.author.db;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String nacionalidad;
+
+    @Column(name = "book_id")
+    private Integer bookId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+}
